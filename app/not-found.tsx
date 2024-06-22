9259c2537deea359b7cb4261171104852fd0aee7
JakeCampbell-dev <155278961+JakeCampbell-dev@users.noreply.github.com>
2024-06-23 09:32:57 +1000

    feat: Add hack for redirecting to correct page in NotFound component
    This commit adds a hack in the NotFound component to redirect the user to the correct page when using a magic link. This hack is only necessary in production and not needed in development. It improves the user experience by ensuring that users are directed to the appropriate page when encountering a 404 error.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,6 +4,10 @@ import { redirect } from "next/navigation";
 import { useSearchParams } from "next/navigation";
 
 const NotFound = () => {
+  // A hack in production to redirect to the correct page
+  // when the user is using a magic link
+  // This is not needed in development
+
   const searchParams = useSearchParams();
   console.log(searchParams);
   if (process.env.NODE_ENV === "production")
